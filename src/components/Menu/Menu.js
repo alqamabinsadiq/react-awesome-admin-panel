fd7e596948f1ba7d9a4d5d065c862012d9db1d58
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2018-07-13 12:36:33 +0500

    Add modal actions

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Menu, Icon } from 'antd';
 import PropTypes from 'prop-types';
-import managerIcon from '../../styles/images/manager.svg';
+// import managerIcon from '../../styles/images/manager.svg';
 class SliderMenu extends Component {
   constructor(props) {
     super(props);
@@ -29,10 +29,6 @@ class SliderMenu extends Component {
         <Menu.Item key="/dashboard/tasks">
           <Icon type="flag" />
           <span>Tasks</span>
-        </Menu.Item>
-        <Menu.Item key="/dashboard/supervisors">
-          <img className="anticon anticon-flag" src={managerIcon} style={{ height: '14px' }} />
-          <span>Supervisors</span>
         </Menu.Item>
         <Menu.Item key="/dashboard/worker-pay-rates">
           <Icon type="pay-circle" />
--- src/components/NumberCard/NumberCard.js
+++ src/components/NumberCard/NumberCard.js
@@ -2,36 +2,25 @@ import React from 'react';
 import { Icon, Card } from 'antd';
 import CountUp from 'react-countup';
 import PropTypes from 'prop-types';
-import Active from '../../styles/images/active.png';
-import Pending from '../../styles/images/pending.png';
-import Complete from '../../styles/images/complete.png';
-import Unassigned from '../../styles/images/notAssigned.png';
-import Leased from '../../styles/images/leased.png';
-import NotLeased from '../../styles/images/notLeased.png';
+// import Active from '../../styles/images/active.png';
+// import Pending from '../../styles/images/pending.png';
+// import Complete from '../../styles/images/complete.png';
+// import Unassigned from '../../styles/images/notAssigned.png';
+// import Leased from '../../styles/images/leased.png';
+// import NotLeased from '../../styles/images/notLeased.png';
 const styles = {
   iconStyle: {
     height: 54
   }
 };
 const NumberCard = (props) => {
-  const { icon, color, title, number, type, link } = props;
+  const { icon, color, title, number, link } = props;
   return (
     <a href={link}>
       <Card loading={number >= 0 ? false : true} className={"numberCard"} bordered={false} bodyStyle={{ padding: 0 }}>
         {icon ?
           <Icon className={"iconWarp"} style={{ color }} type={icon} />
           : null}
-        {type ?
-          type == 'active' ?
-            <img style={styles.iconStyle} src={Active} className={"iconWarp"} /> :
-            type == 'pending' ? <img className={"iconWarp"} src={Pending} style={styles.iconStyle} /> :
-              type == 'complete' ?
-                <img src={Complete} className={"iconWarp"} style={styles.iconStyle} /> :
-                type == 'unassigned' ? <img src={Unassigned} className={"iconWarp"} style={styles.iconStyle} /> :
-                  type == 'leased' ? <img src={Leased} className={"iconWarp"} style={styles.iconStyle} /> :
-                    type == 'notLeased' ? <img src={NotLeased} className={"iconWarp"} style={styles.iconStyle} /> : null
-          : null
-        }
         <div className={"content"}>
           <p className={"title"}>{title || 'No Title'}</p>
           <p className={"number"}>
