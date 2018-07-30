ff46beace8c6c4200d8d839403d32808083ed286
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2018-07-30 11:50:53 +0500

    create Notification container

---
+++ src/containers/Notification/Notification.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+// Components
+import Notifications from 'react-notification-system-redux';
+
+// Styles
+const style = {
+  NotificationItem: { // Override the notification item
+    DefaultStyle: { // Applied to every notification, regardless of the notification level
+      margin: '10px 5px 2px 1px'
+    }
+  }
+};
+
+class Notification extends Component {
+  render() {
+    return (
+      <Notifications
+        notifications={this.props.notifications}
+        style={style}
+      />
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  notifications: state.notifications
+});
+
+export default connect(mapStateToProps)(Notification);
