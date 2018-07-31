39234514c6e985cb5dc1930c18a3536a1a99686a
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2018-07-31 17:33:03 +0500

    refactoring

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -3,7 +3,7 @@ import { Button, Row, Form, Input } from 'antd';
 // import { login } from '../../actions/user';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-// import Notifications from '../Notification/NotificationsContainer';
+import Notifications from '../Notification/Notification';
 // Logo
 import Logo from '../../styles/images/logo.png';
 const FormItem = Form.Item;
@@ -24,41 +24,53 @@ class LoginForm extends Component {
     });
   }
 
+  // Form
+  static Form = ({ form, loading }) => {
+    const { getFieldDecorator } = form;
+
+    return <Form onSubmit={this.handleSubmit}>
+      <FormItem hasFeedback>
+        {getFieldDecorator('username', {
+          rules: [
+            {
+              required: true,
+              message: 'Please Enter Your Email'
+            }
+          ]
+        })(<Input size='large' placeholder='Email' />)}
+      </FormItem>
+      <FormItem hasFeedback>
+        {getFieldDecorator('password', {
+          rules: [
+            {
+              required: true,
+              message: 'Please Enter Your Password'
+            }
+          ]
+        })(<Input size='large' type='password' placeholder='Password' />)}
+      </FormItem>
+      <Row>
+        <Button type="primary" htmlType="submit" size="large" loading={loading}>
+          Login
+        </Button>
+      </Row>
+    </Form>
+  }
+
+  // logo and title
+  static Logo = () => {
+    return <div className="loginLogo">
+      <img src={Logo} alt="logo" />
+      <span>Awesome Admin Panel</span>
+    </div>
+  }
+
   render() {
-    const { getFieldDecorator } = this.props.form;
     return (
       <div className="loginForm">
-        <div className="loginLogo">
-          <img src={Logo} alt="logo" />
-          <span>Awesome Admin Panel</span>
-        </div>
-        <Form onSubmit={this.handleSubmit}>
-          <FormItem hasFeedback>
-            {getFieldDecorator('username', {
-              rules: [
-                {
-                  required: true,
-                  message: 'Please Enter Your Email'
-                }
-              ]
-            })(<Input size='large' placeholder='Email' />)}
-          </FormItem>
-          <FormItem hasFeedback>
-            {getFieldDecorator('password', {
-              rules: [
-                {
-                  required: true,
-                  message: 'Please Enter Your Password'
-                }
-              ]
-            })(<Input size='large' type='password' placeholder='Password' />)}
-          </FormItem>
-          <Row>
-            <Button type="primary" htmlType="submit" size="large" loading={this.state.loginButtonLoading}>
-              Login
-            </Button>
-          </Row>
-        </Form>
+        <Notifications />
+        <LoginForm.Logo />
+        <LoginForm.Form form={this.props.form} loading={this.state.loginButtonLoading} />
       </div>
     );
   }
