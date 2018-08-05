40cbc034b4f402d5d351747aa9364965f139e5b5
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2018-08-05 16:38:18 +0500

    create routes file

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+// import Dashboard from '../';
+
+// // router configuration.
+// export default [
+//   {
+//     path: '/dashboard',
+//     name: 'Project Dashboard',
+//     component: Dashboard,
+//     exact: true,
+//     strict: true
+//   }
+// ];
