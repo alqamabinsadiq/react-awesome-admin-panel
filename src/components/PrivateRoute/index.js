712078fd09b2e5f8308d367c5bcc253cce05f659
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2018-10-08 16:03:24 +0500

    create Public Router component.

--- src/components/PrivateRoute/index.js
+++ src/components/PrivateRoute/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+// import { setExistingUserOnLogRocket } from '../../actions/user';
 
 export const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route {...rest} render={props => (
-    sessionStorage.getItem('token')
-      ? <Component {...props} />
-      : <Redirect to={{ pathname: '/', state: { from: props.location } }} />
-  )} />
+  <Route {...rest} render={props => {
+    if (localStorage.getItem('token')) {
+      // setExistingUserOnLogRocket();
+      return <Component {...props} />;
+    }
+    return <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+  }
+  }
+  />
 );
+PrivateRoute.propTypes = {
+  location: PropTypes.object,
+  component: PropTypes.any
+}
