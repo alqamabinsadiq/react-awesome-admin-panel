ca4f0dc2bca5f8508b6a142ea62f582457837f99
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2018-10-09 22:44:54 +0500

    create separate routes file for sub routes.

---
+++ src/containers/routes.js
@@ -0,0 +1,16 @@
+// import App from './App';
+import Dashboard from './Dashboard/Dashboard';
+// import UnitExplorer from '../Explorer/UnitExplorer';
+
+// router configuration.
+let routes = [
+  {
+    path: '/dashboard',
+    name: 'Project Dashboard',
+    component: Dashboard,
+    exact: true,
+    strict: true
+  }
+];
+
+export default routes;
