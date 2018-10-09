37a9809cac7d95d8d6921b32332dda9f31e5c6f4
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2018-10-09 22:45:11 +0500

    Integrate NumebrCards to dashbaord.

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'antd';
 import NumberCard from '../../components/NumberCard/NumberCard';
 
 class Dashboard extends Component {
+  cards = [
+    {
+      color: '#8fd684f5',
+      number: 1033,
+      type: 'user-add'
+    },
+    {
+      color: '#ffb2b2',
+      number: 3233,
+      type: 'user-delete'
+    },
+    {
+      color: '#dabb5b',
+      number: 999,
+      type: 'usergroup-add'
+    },
+  ]
   render() {
     return (
-      <div></div>
+      <Row gutter={27}>
+        {
+          this.cards.map((item, index) => {
+            return (
+              <Col span={8} key={index}>
+                <NumberCard color={item.color} number={item.number} type={item.type} />
+              </Col>
+            );
+          })
+        }
+      </Row>
     );
   }
 }
