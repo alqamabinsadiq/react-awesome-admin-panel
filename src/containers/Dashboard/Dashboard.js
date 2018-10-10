f104baa941748225bc46fc5f8074675b55fa369c
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2018-10-10 21:48:47 +0500

    Add chart section

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'antd';
+// import { Chart } from 'chart.js';
 import NumberCard from '../../components/NumberCard/NumberCard';
+import BubbleChart from '../../components/Charts/Bubble';
+import PolarChart from '../../components/Charts/Polar';
+import Doughnut from '../../components/Charts/Doughnut';
 
 class Dashboard extends Component {
   cards = [
@@ -21,18 +25,38 @@ class Dashboard extends Component {
     },
   ]
   render() {
+    // const Bubble = new Chart()
     return (
-      <Row gutter={27}>
-        {
-          this.cards.map((item, index) => {
-            return (
-              <Col span={8} key={index}>
-                <NumberCard color={item.color} number={item.number} type={item.type} />
-              </Col>
-            );
-          })
-        }
-      </Row>
+      <div>
+        <Row gutter={27}>
+          {
+            this.cards.map((item, index) => {
+              return (
+                <Col span={8} key={index}>
+                  <NumberCard color={item.color} number={item.number} type={item.type} />
+                </Col>
+              );
+            })
+          }
+        </Row>
+        <Row gutter={27}>
+          <Col span={8}>
+            <div>
+              <BubbleChart />
+            </div>
+          </Col>
+          <Col span={8}>
+            <div>
+              <PolarChart />
+            </div>
+          </Col>
+          <Col span={8}>
+            <div>
+              <Doughnut />
+            </div>
+          </Col>
+        </Row>
+      </div>
     );
   }
 }
