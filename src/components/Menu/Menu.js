267bf771910f12597970c471d2e2f8236837ec4b
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2018-10-11 23:52:20 +0500

    Add onSelect on Menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import { Menu, Icon } from 'antd';
+import PropTypes from 'prop-types';
 
 // Sider Menu Component
-export default (props) =>
-  <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-    <Menu.Item key="dashboard">
-      <Icon type="pie-chart" />
-      <span>Dashboard</span>
-    </Menu.Item>
-    <Menu.Item key="2">
-      <Icon type="video-camera" />
-      <span>nav 2</span>
-    </Menu.Item>
-    <Menu.Item key="3">
-      <Icon type="upload" />
-      <span>nav 3</span>
-    </Menu.Item>
-  </Menu>
+const SideMenu = ({ onMenuSelect }) => {
+  return (
+    <Menu
+      theme="dark"
+      mode="inline"
+      defaultSelectedKeys={['1']}
+      onSelect={({ key }) => { onMenuSelect(key); }}
+      selectedKeys={[window.location.pathname]}
+    >
+      <Menu.Item key="/dashboard">
+        <Icon type="pie-chart" />
+        <span>Dashboard</span>
+      </Menu.Item>
+      <Menu.Item key="/dashboard/table">
+        <Icon type="video-camera" />
+        <span>nav 2</span>
+      </Menu.Item>
+      <Menu.Item key="3">
+        <Icon type="upload" />
+        <span>nav 3</span>
+      </Menu.Item>
+    </Menu>
+  );
+}
+
+SideMenu.propTypes = {
+  onMenuSelect: PropTypes.func
+}
+
+export default SideMenu;
