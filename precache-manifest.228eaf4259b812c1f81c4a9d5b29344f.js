10b278aca3f8e43eaba3eb35097aaf95a44451db
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2018-10-17 23:24:57 +0500

    Updates

---
+++ precache-manifest.228eaf4259b812c1f81c4a9d5b29344f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2ffb3a32c2e07826c887815599de6f3",
+    "url": "/react-awesome-admin-panel/static/media/logo.b2ffb3a3.png"
+  },
+  {
+    "revision": "21b3f2700046c5ad0195",
+    "url": "/react-awesome-admin-panel/static/js/runtime~main.21b3f270.js"
+  },
+  {
+    "revision": "62efbab13eb0c69c9114",
+    "url": "/react-awesome-admin-panel/static/js/main.62efbab1.chunk.js"
+  },
+  {
+    "revision": "da598d4bb0529f90b83c",
+    "url": "/react-awesome-admin-panel/static/js/1.da598d4b.chunk.js"
+  },
+  {
+    "revision": "62efbab13eb0c69c9114",
+    "url": "/react-awesome-admin-panel/static/css/main.79953451.chunk.css"
+  },
+  {
+    "revision": "da598d4bb0529f90b83c",
+    "url": "/react-awesome-admin-panel/static/css/1.e376a798.chunk.css"
+  },
+  {
+    "revision": "53a59a3735446246c12f8d75b2717854",
+    "url": "/react-awesome-admin-panel/index.html"
+  }
+];
