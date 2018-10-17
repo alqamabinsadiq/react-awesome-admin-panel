f8b9e7c59ce3d09a86c73a9623e55614262dbe1f
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2018-10-17 23:15:30 +0500

    Create Charts container.

---
+++ src/containers/Charts/Charts.index.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Row, Col } from 'antd';
+import BubbleChart from '../../components/Charts/Bubble';
+import PolarChart from '../../components/Charts/Polar';
+import Doughnut from '../../components/Charts/Doughnut';
+import Bar from '../../components/Charts/Bar';
+// import './Dashboard.style.scss';
+
+class Charts extends Component {
+
+  static ChartSection = () => {
+    return (
+      <Row gutter={27} style={{ marginTop: '3rem', height: 300 }}>
+        <Col span={8}>
+          <div>
+            <BubbleChart />
+          </div>
+        </Col>
+        <Col span={8}>
+          <div>
+            <PolarChart />
+          </div>
+        </Col>
+        <Col span={8}>
+          <div>
+            <Doughnut />
+          </div>
+        </Col>
+      </Row>
+    );
+  }
+
+  static BarChart = () => <Row gutter={27} style={{ margin: '3rem', height: 400 }}><Bar /></Row>
+
+
+  render() {
+    // const Bubble = new Chart()
+    return (
+      <div>
+        <Charts.ChartSection />
+        <Charts.BarChart />
+      </div>
+    );
+  }
+}
+
+export default Charts;
