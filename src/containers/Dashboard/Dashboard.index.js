e8f8012987b66245ee3b70e7884c0719addbe5a6
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2018-10-18 20:14:42 +0500

    Create Stats section and add progress bard in it

--- src/containers/Dashboard/Dashboard.index.js
+++ src/containers/Dashboard/Dashboard.index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, Card, Progress } from 'antd';
 // import { Chart } from 'chart.js';
 import NumberCard from '../../components/NumberCard/NumberCard';
 import BubbleChart from '../../components/Charts/Bubble';
@@ -11,16 +11,19 @@ import './Dashboard.style.scss';
 class Dashboard extends Component {
   cards = [
     {
+      title: '',
       color: '#8fd684f5',
       number: 1033,
       type: 'user-add'
     },
     {
+      title: '',
       color: '#ffb2b2',
       number: 3233,
       type: 'user-delete'
     },
     {
+      title: '',
       color: '#dabb5b',
       number: 999,
       type: 'usergroup-add'
@@ -68,11 +71,53 @@ class Dashboard extends Component {
   static BarChart = () => <Row gutter={27} style={{ margin: '3rem', height: 400 }}><Bar /></Row>
 
 
+  static ProgressBar = () => {
+    return (
+      <Card hoverable>
+        <div className="statsWithProgressBar">
+          <div className="header">
+            <span className="headerNum">3201</span> <span className="statsHeaderTitle">Progress</span>
+          </div>
+          <div style={{ marginTop: 15 }}>
+            <div>
+              January
+                <Progress percent={60} strokeColor='#ffc920b3' format={percent => `${percent}`} />
+            </div>
+            <div>
+              Febuary
+                <Progress percent={60} strokeColor='#ff6666f5' format={percent => `${percent}`} />
+            </div>
+            <div>
+              March
+                <Progress percent={40} strokeColor='#7edacf' format={percent => `${percent}`} />
+            </div>
+          </div>
+        </div>
+      </Card>
+    );
+  }
+  static Stats = () => {
+    return (
+      <Row gutter={27} style={{ marginTop: 10 }}>
+        <Col span={8}>
+          <Card hoverable></Card>
+        </Col>
+        <Col span={8}>
+          <Card hoverable></Card>
+        </Col>
+        <Col span={8}>
+          <Dashboard.ProgressBar />
+        </Col>
+      </Row>
+    );
+  }
+
   render() {
     // const Bubble = new Chart()
     return (
       <div>
         <Dashboard.NumberCards cards={this.cards} />
+        <Dashboard.Stats />
         <Dashboard.ChartSection />
         <Dashboard.BarChart />
       </div>
