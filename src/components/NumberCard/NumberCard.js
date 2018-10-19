3adf5fc4c1d9aead3293498c0f6011f0ca319703
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2018-10-19 12:01:13 +0500

    Modify Number Card to add title.

--- src/components/NumberCard/NumberCard.js
+++ src/components/NumberCard/NumberCard.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { Card, Icon } from 'antd';
 import CountUp from 'react-countup';
 import PropTypes from 'prop-types';
+import './NumberCard.style.scss';
 
-const NumberCard = ({ color, number, type }) => {
+const NumberCard = ({ color, number, type, title }) => {
   return (
     <Card hoverable>
-      <div className="dashboardCard">
+      <div className="raap-number-card">
         <div className="icon">
           <Icon type={type} style={{ color: color }} />
         </div>
         <div className="count">
+          <div className="raap-count-title">{title}</div>
           <CountUp end={number} />
         </div>
       </div>
@@ -22,6 +24,7 @@ NumberCard.propTypes = {
   color: PropTypes.string,
   type: PropTypes.string,
   number: PropTypes.string,
+  title: PropTypes.string,
 }
 
 export default NumberCard;
