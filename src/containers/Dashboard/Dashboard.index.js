7bc8eea323d7579b3d908f1d99b12a852d0e4c1e
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2018-10-21 22:50:07 +0500

    Create Timeline component.

--- src/containers/Dashboard/Dashboard.index.js
+++ src/containers/Dashboard/Dashboard.index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col, Card, Progress, Badge } from 'antd';
+import { Row, Col, Card, Progress, Badge, Timeline } from 'antd';
 import NumberCard from '../../components/NumberCard/NumberCard';
 // import { Doughnut } from '../../components/Charts/Doughnut';
 import Bar from '../../components/Charts/Bar';
@@ -131,7 +131,7 @@ class Dashboard extends Component {
   static CircularProgressCard = ({ title, count, strokeColor, progress, headerBackground, bodyColor }) => {
     return (
       <Col span={5} style={{ padding: 0 }}>
-        <Card hoverable style={{ display: 'flex', flexDirection: 'column', padding: 0 }} bodyStyle={{ padding: 0 }} >
+        <Card hoverable style={{ display: 'flex', flexDirection: 'column', padding: 0, maxHeight: 300 }} bodyStyle={{ padding: 0 }} >
           <div style={{ display: 'flex', width: '100%', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', color: 'white', height: 60, backgroundColor: headerBackground }}>
             <div>{title}</div>
             <div>{count}</div>
@@ -144,6 +144,35 @@ class Dashboard extends Component {
     );
   }
 
+  static TimeLineComponent = ({ title, headerBackground }) => {
+    return (
+      <Card hoverable style={{ maxHeight: 300, display: 'flex', flexDirection: 'column', padding: 0 }} bodyStyle={{ padding: 0 }}>
+        <div className="timeline-card-component">
+          <div style={{ backgroundColor: headerBackground }} className="header">
+            <div>{title}</div>
+          </div>
+          <div style={{ overflow: 'scroll', maxHeight: 240, padding: 10, paddingLeft: 15 }}>
+            <Timeline>
+              <Timeline.Item color="green">Create a services site 2015-09-01</Timeline.Item>
+              <Timeline.Item color="green">Create a services site 2015-09-01</Timeline.Item>
+              <Timeline.Item color="red">
+                <p>Solve initial network problems 1</p>
+                <p>Solve initial network problems 2</p>
+                <p>Solve initial network problems 3 2015-09-01</p>
+              </Timeline.Item>
+              <Timeline.Item>
+                <p>Technical testing 1</p>
+                <p>Technical testing 2</p>
+                <p>Technical testing 3 2015-09-01</p>
+              </Timeline.Item>
+            </Timeline>
+          </div>
+
+        </div>
+      </Card>
+    );
+  }
+
   static StatsWithTimeline = () => {
     return (
       <Row gutter={24} style={{ margin: 1, marginTop: 30, height: 500 }}>
@@ -151,7 +180,7 @@ class Dashboard extends Component {
         <Dashboard.CircularProgressCard title="Total Projects" count={900} progress={45} strokeColor="7edacf" headerBackground="#7edacf" />
         <Dashboard.CircularProgressCard title="Total Projects" count={1700} progress={60} strokeColor="ffc920b3" headerBackground="#ffc920b3" />
         <Col span={9} style={{ padding: 0 }}>
-          <Card style={{ width: '100%' }}>Alqama</Card>
+          <Dashboard.TimeLineComponent headerBackground="#f1f1f1" title="Project Activity" />
         </Col>
       </Row>
     );
