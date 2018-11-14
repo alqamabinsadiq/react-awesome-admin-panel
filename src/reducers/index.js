71e2bfd093ee78975d4f32c7f4ef5c9a0dcab3b3
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2018-11-14 12:42:58 +0500

    Add users to combine reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import users from './user';
 
 // Root Reducer.
 export default combineReducers({
   routing: routerReducer,
+  users
 });
