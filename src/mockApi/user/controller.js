e949e16f8f51f51b4a4c28d019feba4fa5739f7f
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2018-11-14 12:42:30 +0500

    Create getUsers mock api func.

--- src/mockApi/user/controller.js
+++ src/mockApi/user/controller.js
@@ -3,10 +3,19 @@ import db from './db.json';
 export const login = (email, password) => {
   for (const user of db) {
     if (email === user.email && password === user.password) {
+      delete user.password;
       return Promise.resolve(user);
     }
     else {
       return Promise.reject('Username or password may be incorrect.');
     }
   }
+}
+
+export const getUsers = () => {
+  const data = db.map((user) => {
+    delete user.password;
+    return user;
+  });
+  setTimeout(() => Promise.resolve(data), 1000);
 }
