5db450b5a6cb4961904798b840554566013e4ac4
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2018-11-14 17:06:20 +0500

    create reducers for allUsers and loading.

--- src/reducers/user/index.js
+++ src/reducers/user/index.js
@@ -2,7 +2,9 @@ import { actions } from '../../actions/user';
 
 // Initial State
 const INITIAL_STATE = {
-  user: null
+  user: null,
+  allUsers: null,
+  loading: false
 };
 
 // User Reducer.
@@ -10,6 +12,10 @@ export default (state = INITIAL_STATE, { type, data }) => {
   switch (type) {
     case actions.SET_USER_INFO:
       return { ...state, user: data };
+    case actions.SET_ALL_USERS:
+      return { ...state, allUsers: data };
+    case actions.SET_USER_LOADER:
+      return { ...state, loading: data };
     default:
       return state;
   }
