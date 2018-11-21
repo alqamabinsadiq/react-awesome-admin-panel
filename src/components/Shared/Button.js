441d69b76e1c5bea5bded1622cc8a12d365debf7
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2018-11-21 15:53:08 +0500

    Add onClick prop

--- src/components/Shared/Button.js
+++ src/components/Shared/Button.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Button as AntdButton, Icon } from 'antd';
 import PropTypes from 'prop-types';
 
-const Button = ({ label, hasIcon = false, type, iconType = null }) => {
+const Button = ({ label, hasIcon = false, type, iconType = null, onClick }) => {
   return (
-    <AntdButton type={type}>
+    <AntdButton type={type} onClick={onClick}>
       {
         hasIcon ? <Icon type={iconType} /> : null
       }
@@ -17,7 +17,8 @@ Button.propTypes = {
   label: PropTypes.string,
   hasIcon: PropTypes.bool,
   type: PropTypes.string,
-  iconType: PropTypes.string
+  iconType: PropTypes.string,
+  onClick: PropTypes.func
 }
 
 export { Button };
